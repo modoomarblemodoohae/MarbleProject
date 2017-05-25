468cac4857ea3816f1edce3975d7ec9922dc9de7
SMCH <SMCH@DESKTOP-8955JH1>
2017-05-25 14:48:22 +0900

    골드카드 추상클래스(완료?)

---
+++ MarbleProject/src/kr/marble/goldcard/GoldCard.java
@@ -0,0 +1,22 @@
+package kr.marble.goldcard;
+
+import kr.marble.building.Building;
+
+public abstract class GoldCard { // 골드 카드 추상클래스
+	
+	private String name;
+	
+	public GoldCard(String name) {
+		this.name = name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public abstract void usingGoldCard(Building building);
+}
