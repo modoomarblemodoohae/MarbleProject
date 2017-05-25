62ba6c40f98ceb64e23e2dfa00388f96fef80d3f
SMCH <SMCH@DESKTOP-8955JH1>
2017-05-25 13:03:41 +0900

    기본틀

--- MarbleProject/src/kr/marble/goldcard/IGoldCard.java
+++ MarbleProject/src/kr/marble/goldcard/IGoldCard.java
@@ -1,5 +1,7 @@
 package kr.marble.goldcard;
 
-public interface IGoldCard { // 골드 카드 인터페이스
+import kr.marble.Player;
 
+public interface IGoldCard { // 골드 카드 인터페이스
+	public void usingGoldCard(Player player); // 플레이어가 골드카드를 사용하였을때 
 }
