07a6fe352fc398ae0b9bd8692976a93b1f7241a3
SEC\user <gmlsdyd1023@naver.com>
2017-05-28 22:14:34 +0900

    플레이어 클래스

--- MarbleProject/src/kr/marble/Player.java
+++ MarbleProject/src/kr/marble/Player.java
@@ -1,5 +1,119 @@
 package kr.marble;
 
-public class Player { // 플레이어 클래스
-
-}
+import java.util.ArrayList;
+import kr.marble.building.Building;
+ 
+public class Player {
+	private Money money; //돈
+	private String name; //이름
+	private int location; //위치
+	private boolean noMoney = false; //플레이어 파산여부
+	private int status = 0;
+	private ArrayList<Building> buildings = new ArrayList<>(); //건물들
+	
+	public static final int MORMAL = 0;
+	public static final int ISLAND = 1;
+	public static final int SPACE_TRAVEL = 2;
+	
+  
+	public Player(String name){ 
+		this.name = name; 
+	} //Player생성자 ////////////////////////////////// 수정 1
+  
+	public void noMoney(){  ///////////////////////// 수정 2
+		this.noMoney = true;
+	} 
+	public Money getMoney(){ /////////////////////////////// 수정 3
+		return money;
+	}
+  
+	public boolean isNoMoney(){
+		return noMoney;
+	} // 플레이어 파산 여부
+  
+	public String getName(){
+		return name;
+	} //이름 리턴
+  
+	public void setName(String name){
+		this.name = name;
+	} //이름 입력 받음
+  
+	public void move(int count){
+		location += count;
+	} //플레이어 count 값에 따라 이동
+	
+	public int getLocation() {
+		return location;
+	}
+	
+	public double getProperty() {
+		Building[] builds = getBuildings();
+		double sum = 0;
+		
+		if(builds.length > 0) {
+			for(Building build : builds)
+				sum += build.getBuyMoney();
+		}
+		
+		sum += money.getMoney();
+		
+		return sum;
+	}
+	
+	public Building[] getBuildings() {
+		Building[] builds = new Building[buildings.size()];
+		
+		if(builds.length == 0) return null;
+		
+		return buildings.toArray(builds);
+	}
+	
+	public boolean hasBuilding(Building build) {
+		for(Building building : buildings) {
+			if(building.equals(build)) return true;
+		}
+		return false;
+	}
+	
+	public boolean addBuilding(Building build) {
+		if(money.getMoney() < build.getBuyMoney()) return false;
+		
+		build.onBuyingBuilding(this);
+		build.setWho(this);
+		money.minusMoney(build.getBuyMoney());
+		buildings.add(build);
+		
+		return true;
+	}
+	
+	public boolean removeBuilding(Building build) {
+		return removeBuilding(build, null);
+	}
+	
+	public boolean removeBuilding(Building build, Player player) {
+		if(!hasBuilding(build)) return false;
+		
+		money.addMoney(build.getBuyMoney());
+		buildings.remove(build);
+		
+		if(player != null) 
+			player.addBuilding(build);
+		
+		return true;
+		
+	}
+	
+	public void cleanBuilding() {
+		buildings.clear();
+	}
+	
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	
+	public int getStatus() {
+		return status;
+	}
+  
+ }
