83a1f045d4f4a900e574102d0d5a48b1d9b232b2
SEC\user <gmlsdyd1023@naver.com>
2017-05-28 22:14:27 +0900

    돈 클래스 (완성)

--- MarbleProject/src/kr/marble/Money.java
+++ MarbleProject/src/kr/marble/Money.java
@@ -1,5 +1,25 @@
 package kr.marble;
 
-public class Money { // µ· Å¬·¡½º
+public class Money {
+	   private double money;
 
+	   public Money(double money) {
+	      this.money = money;
+	   }
+
+	   public double getMoney() {
+	      return money;
+	   }
+
+	   public void setMoney(double money) {
+	      this.money = money;
+	   }
+
+	   public void minusMoney(double money) {
+	      this.money -= money;
+	   }
+
+	   public void addMoney(double money) {
+	      this.money += money;
+	   }
 }
