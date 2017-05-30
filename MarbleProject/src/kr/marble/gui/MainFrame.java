f810fb2b7aa3417b6918370373510041e849c142
SMCH <SMCH@DESKTOP-8955JH1>
2017-05-30 16:22:34 +0900

    업데이트

--- MarbleProject/src/kr/marble/gui/MainFrame.java
+++ MarbleProject/src/kr/marble/gui/MainFrame.java
@@ -1,22 +1,84 @@
 package kr.marble.gui;
 
+import java.awt.Container; 
+import java.io.File;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class MainFrame extends JFrame {
 	
-	private JButton[] btns = new JButton[32];
 	private JButton okBtn, noBtn, diceBtn;
 	
 	private JTextField[] fields = new JTextField[4];
 	private JLabel[] labels = new JLabel[4];
 	
-	public MainFrame() {
-		super("-- 부루마블 게임 --");
+	private JButton button[] = new JButton[32];
+	
+	private final static int WIDTH = 70;
+	private final static int HEIGHT = 140;
+	
+	private int x = 1000;
+	private int y = 700; 
+	
+	public MainFrame(){
+		button = new JButton[32];
+		setTitle("asd");
+		setSize(1500,900);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		setSize(1200, 900);
+		Container c= getContentPane();
+		c.setLayout(null);
+		
+		for(int i = 0; i < 4; i++) {
+			for(int j = i * (button.length / 4); j <= (i * (button.length / 4)) + 7; j ++){
+				button[j] = new JButton(new ImageIcon("C:/workspace/MarbleProject/image/" + (j + 1) + ".png"));
+				button[j].setBorderPainted(false);
+				button[j].setContentAreaFilled(false);
+				//button[j].setFocusPainted(false);
+				if(i == 0) {
+					if(j == 0)
+						button[j].setBounds(x, y, HEIGHT, HEIGHT);
+					else
+						button[j].setBounds(x, y, WIDTH, HEIGHT);
+					if(j == 7)
+						x -= HEIGHT;
+					else
+						x -= WIDTH;
+				} else if(i == 1) {
+					if(j == 8)
+						button[j].setBounds(x, y, HEIGHT, HEIGHT);
+					else 
+						button[j].setBounds(x, y, HEIGHT, WIDTH);
+					if(j == 15) 
+						y -= HEIGHT;
+					else
+						y -= WIDTH;
+				} else if(i == 2) {
+					if(j == 16) {
+						button[j].setBounds(x, y, HEIGHT, HEIGHT);
+						x += HEIGHT;
+					} else {
+						button[j].setBounds(x, y, WIDTH, HEIGHT);
+						x += WIDTH;
+					}
+				} else {
+					if(j == 24) {
+						button[j].setBounds(x, y, HEIGHT, HEIGHT);
+						y += HEIGHT;
+					} else {
+						button[j].setBounds(x, y, HEIGHT, WIDTH);
+						y += WIDTH;
+					}
+				}
+				add(button[j]);
+			}
+		}
+		
 		setVisible(true);
 	}
 	
