4d677746ce631dbb148cedfd99d439ffbc0b8f64
SMCH <SMCH@DESKTOP-8955JH1>
2017-05-30 16:22:28 +0900

    골드카드 업데이트

--- MarbleProject/src/kr/marble/goldcard/GoldCard.java
+++ MarbleProject/src/kr/marble/goldcard/GoldCard.java
@@ -6,7 +6,7 @@ import kr.marble.building.Building;
 public class GoldCard { // 골드 카드 추상클래스
 	
 	private String name; // 카드  이름
-	private boolean needBuilding = false;
+	private boolean needBuilding = false; // 건물이 이용되는가
 	private boolean needPlayer = false;
 	
 	public GoldCard(String name, boolean needBuilding, boolean needPlayer) {
