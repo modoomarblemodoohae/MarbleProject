0c358e480169c0fcf27aab5b98a93598db621f20
SMCH <SMCH@DESKTOP-8955JH1>
2017-05-31 10:36:59 +0900

    게임관리 이벤트 리스너 작성

---
+++ MarbleProject/src/kr/marble/IManagerListener.java
@@ -0,0 +1,9 @@
+package kr.marble;
+
+public interface IManagerListener {
+	public void onJoinIsLand(Player player);
+	public void onGiveDonate(Player player);
+	public void onSpaceTravel(Player player);
+	public void onReceiveDonate(Player player, double donate);
+	public void onNoMoneyPlayer(Player player);
+}
